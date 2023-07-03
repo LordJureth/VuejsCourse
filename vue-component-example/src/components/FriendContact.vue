171fd3105a7524b6d834df182986708b8bb8e056
“Toby <toby.loudon@clickdealer.co.uk>
2023-07-03 12:29:28 +0100

    vuejs component example

---
+++ vue-component-example/src/components/FriendContact.vue
@@ -0,0 +1,39 @@
+<template>
+    <li>
+      <h2>{{ friend.name }}</h2>
+      <button v-on:click="toggleDetails()">{{ detailsButtonText }}</button>
+      <ul v-if="detailsAreVisible">
+        <li>
+          <strong>Phone:</strong>
+          {{ friend.phone }}
+        </li>
+        <li>
+          <strong>Email:</strong>
+          {{ friend.email }}
+        </li>
+      </ul>
+    </li>
+  </template>
+  
+  <script>
+  export default {
+    props: [
+        'friend',
+    ],
+    data() {
+      return {
+        detailsAreVisible: false,
+      };
+    },
+    computed: {
+        detailsButtonText: function () {
+            return !this.detailsAreVisible ? 'Show Details' : 'Hide Details';
+        },
+    },
+    methods: {
+      toggleDetails() {
+        this.detailsAreVisible = !this.detailsAreVisible;
+      },
+    }
+  };
+  </script>
