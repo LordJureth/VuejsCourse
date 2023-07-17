22ce9c004167532f708ab94c5f42a00964483581
“Toby <toby.loudon@clickdealer.co.uk>
2023-07-17 16:00:12 +0100

    advanced components - dynamic

--- advanced-components-slots/src/App.vue
+++ advanced-components-slots/src/App.vue
@@ -1,36 +1,28 @@
 <template>
   <div>
     <the-header></the-header>
-    <badge-list></badge-list>
-    <user-info
-      :full-name="activeUser.name"
-      :info-text="activeUser.description"
-      :role="activeUser.role"
-    ></user-info>
-    <base-card>
-        <course-goals #default="slotProps">
-            <h2>{{ slotProps.item }}</h2>
-            <p>{{ slotProps['anotherProp'] }}</p>
-        </course-goals>
-    </base-card>
+    <button @click="setSelectedComponent('active-goals')">Active Goals</button>
+    <button @click="setSelectedComponent('manage-goals')">Manage Goals</button>
+    <keep-alive>
+        <component :is="selectedComponent"></component>
+    </keep-alive>
   </div>
 </template>
 
 <script>
 import TheHeader from './components/TheHeader.vue';
-import BadgeList from './components/BadgeList.vue';
-import UserInfo from './components/UserInfo.vue';
-import CourseGoals from './components/CourseGoals.vue';
+import ActiveGoals from './components/ActiveGoals.vue';
+import ManageGoals from './components/ManageGoals.vue';
 
 export default {
     components: {
         TheHeader,
-        BadgeList,
-        UserInfo,
-        CourseGoals
+        ActiveGoals,
+        ManageGoals
     },
     data() {
         return {
+            selectedComponent: 'active-goals',
             activeUser: {
                 name: 'Toby Loudon',
                 description: 'Site owner and admin',
@@ -38,6 +30,11 @@ export default {
             },
         };
     },
+    methods: {
+        setSelectedComponent(component) {
+            this.selectedComponent = component;
+        }
+    }
 };
 </script>
 
